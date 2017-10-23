d827b0ab78b339c6d685b307a985225293756a2b
belykhvd <31358836+belykhvd@users.noreply.github.com>
2017-10-23 21:58:38 +0500

    Hometask1.Testing

---
+++ NumberValidatorTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace HomeExercises
+{    
+	public class NumberValidatorTests
+	{
+        // NUnit 3.0+ does not have ExpectedException in TestCase
+        [TestCase(-1, 2, true)]
+        [TestCase(-1, 2, false)]
+        public static void AssertThrows(int precision, int scale, bool onlyPositive)
+	        => Assert.Throws<ArgumentException>(() => new NumberValidator(precision, scale, onlyPositive));
+
+	    [TestCase(1, 0, true)]	    
+	    public static void AssertDoesNotThrow(int precision, int scale, bool onlyPositive)
+	        => Assert.DoesNotThrow(() => new NumberValidator(precision, scale, onlyPositive));
+
+        [TestCase(3, 2, true, "-0.00", ExpectedResult = false)]
+	    [TestCase(3, 2, true, "+0.00", ExpectedResult = false)]
+	    [TestCase(3, 2, true, "00.00", ExpectedResult = false)]
+        [TestCase(3, 2, true, "+1.23", ExpectedResult = false)]
+	    [TestCase(3, 2, true, "-1.23", ExpectedResult = false)]
+	    [TestCase(3, 2, true, "a.sd", ExpectedResult = false)]
+        [TestCase(4, 2, true, "+1.23", ExpectedResult = true)]
+        [TestCase(17, 2, true, "0", ExpectedResult = true)]
+        [TestCase(17, 2, true, "0.0", ExpectedResult = true)]
+        [TestCase(17, 2, true, "0.000", ExpectedResult = false)]             
+        public static bool ValidateNumber(int precision, int scale, bool onlyPositive, string value)
+	        => new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
+	}    
+
+	public class NumberValidator
+	{
+		private readonly Regex numberRegex;
+		private readonly bool onlyPositive;
+		private readonly int precision;
+		private readonly int scale;
+
+		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
+		{
+			this.precision = precision;
+			this.scale = scale;
+			this.onlyPositive = onlyPositive;
+			if (precision <= 0)
+				throw new ArgumentException("precision must be a positive number");
+			if (scale < 0 || scale >= precision)
+				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
+			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+		}
+
+		public bool IsValidNumber(string value)
+		{
+			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
+			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
+			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
+			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+
+			if (string.IsNullOrEmpty(value))
+				return false;
+
+			var match = numberRegex.Match(value);
+			if (!match.Success)
+				return false;
+
+			// Знак и целая часть
+			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+			// Дробная часть
+			var fracPart = match.Groups[4].Value.Length;
+
+			if (intPart + fracPart > precision || fracPart > scale)
+				return false;
+
+			if (onlyPositive && match.Groups[1].Value == "-")
+				return false;
+			return true;
+		}
+	}
+}
---
+++ ObjectComparison.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace HomeExercises
+{
+	public class ObjectComparison
+	{
+		[Test]
+		[Description("Проверка текущего царя")]
+		[Category("ToRefactor")]
+		public void CheckCurrentTsar()
+		{
+			var actualTsar = TsarRegistry.GetCurrentTsar();
+
+			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+
+		    actualTsar.ShouldBeEquivalentTo(expectedTsar,
+		        options => options.Excluding(person => person.SelectedMemberPath.EndsWith("Id")));
+
+		    /* 
+             * Недостатки альтернативного решения:
+             * 1. Новое свойство порождает +1 строку кода на проверку равенства
+             *   => N новых свойств отнимут время, потенциально увеличат кол-во ошибок, снизят читаемость кода.
+             * 2. Глубина рекурсии явно не ограничена (не параметризуется).
+             */
+		}
+
+        [Test]
+		[Description("Альтернативное решение. Какие у него недостатки?")]
+		public void CheckCurrentTsar_WithCustomEquality()
+		{
+			var actualTsar = TsarRegistry.GetCurrentTsar();
+			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+			new Person("Vasili III of Russia", 28, 170, 60, null));
+
+			// Какие недостатки у такого подхода? 
+			Assert.True(AreEqual(actualTsar, expectedTsar));
+
+		}
+
+		private bool AreEqual(Person actual, Person expected)
+		{
+			if (actual == expected) return true;
+			if (actual == null || expected == null) return false;
+			return
+			actual.Name == expected.Name
+			&& actual.Age == expected.Age
+			&& actual.Height == expected.Height
+			&& actual.Weight == expected.Weight
+			&& AreEqual(actual.Parent, expected.Parent);
+		}
+	}
+
+	public class TsarRegistry
+	{
+		public static Person GetCurrentTsar()
+		{
+			return new Person(
+				"Ivan IV The Terrible", 54, 170, 70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+		}
+	}
+
+	public class Person
+	{
+		public static int IdCounter = 0;
+		public int Age, Height, Weight;
+		public string Name;
+		public Person Parent;
+		public int Id;
+
+		public Person(string name, int age, int height, int weight, Person parent)
+		{
+			Id = IdCounter++;
+			Name = name;
+			Age = age;
+			Height = height;
+			Weight = weight;
+			Parent = parent;
+		}
+	}
+}
